eaab8cbd29f84ed9823604669ec32381d1cb1c3c
rachit0chaudhary <rachitchoudary@gmail.com>
2024-08-30 05:27:01 +0530

    update

--- routes/coupon.js
+++ routes/coupon.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Import the controller
-const { getActiveCoupons } = require('../controllers/Coupon/getAllCoupon');
+const getActiveCoupons = require('../controllers/Coupon/getAllCoupon');
 
 // Route to get all active coupons
 router.get('/allcoupons', getActiveCoupons);  // Ensure route matches expected endpoint
